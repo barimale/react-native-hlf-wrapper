c98e96c6d014b5fdaa117042dd2c69ed0ce4d3ff
Antorweep Chakravorty <chakravrty.antorweep@gmail.com>
2020-06-05 16:35:43 +0200

    testing

--- ios/HlfWrapper.m
+++ ios/HlfWrapper.m
@@ -8,12 +8,12 @@ RCT_REMAP_METHOD(hello,
                  resolver: (RCTPromiseResolveBlock)resolve
                  rejecter: (RCTPromiseRejectBlock)reject)
 {
-  NSArray *result = ...
+  NSError *error;
+  NSString *result = [NSString hello error:&error];
   if (result) {
     resolve(result);
   } else {
-    NSError *error = ...
-    reject(@"no_events", @"E_GET_ERROR", error);
+    reject(@"E_GET_ERROR", error);
   }
 }
 
