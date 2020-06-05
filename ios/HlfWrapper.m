86c556164aeda626fd6d1449e9f5f841f9f60e8f
Antorweep Chakravorty <chakravrty.antorweep@gmail.com>
2020-06-05 15:18:51 +0200

    testing ios native call

--- ios/HlfWrapper.m
+++ ios/HlfWrapper.m
@@ -1,14 +1,19 @@
 #import "HlfWrapper.h"
-
+#import <React/RCTLog.h>
 
 @implementation HlfWrapper
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD((NSString) getName)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  return "HlfWrapper";
 }
+
+// RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+// {
+//     // TODO: Implement some actually useful functionality
+//     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+// }
 
 @end
