bad767f32b55673aacf75127c24a41fac16a5fb9
Antorweep Chakravorty <chakravrty.antorweep@gmail.com>
2020-06-05 16:01:26 +0200

    testing

--- ios/HlfWrapper.m
+++ ios/HlfWrapper.m
@@ -4,16 +4,20 @@
 @implementation HlfWrapper
 
 RCT_EXPORT_MODULE()
-
-// RCT_EXPORT_METHOD((NSString) getName)
-// {
-//   return "HlfWrapper";
-// }
-
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_REMAP_METHOD(sampleMethod,
+                 resolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  NSString *result = @"Hello World!";
+  resolve(result);
 }
+
+// RCT_EXPORT_METHOD(getName)
+
+// RCT_EXPORT_METHOD(sampleMethod:(RCTResponseSenderBlock)callback)
+// {
+//     // TODO: Implement some actually useful functionality
+//     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+// }
 
 @end
